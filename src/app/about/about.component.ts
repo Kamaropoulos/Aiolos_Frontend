1644d0c8df89517e8718d17f3a09ce16c690496d
Konstantinos Kamaropoulos <kamaropoulos@outlook.com>
2020-01-07 00:49:59 +0200

    feat: Add Google Map for testing purposes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, OnInit } from '@angular/core';
 
 import { environment } from '@env/environment';
+
+declare var google: any;
 
 @Component({
   selector: 'app-about',
@@ -9,6 +11,33 @@ import { environment } from '@env/environment';
 })
 export class AboutComponent implements OnInit {
   version: string | null = environment.version;
+
+  @ViewChild('mapContainer', { static: false }) gmap: ElementRef;
+  map: google.maps.Map;
+  lat = 40.73061;
+  lng = -73.935242;
+
+  coordinates = new google.maps.LatLng(this.lat, this.lng);
+
+  mapOptions: google.maps.MapOptions = {
+    center: this.coordinates,
+    zoom: 8
+  };
+
+  marker = new google.maps.Marker({
+    position: this.coordinates,
+    map: this.map,
+    title: 'Hello World!'
+  });
+
+  ngAfterViewInit() {
+    this.mapInitializer();
+  }
+
+  mapInitializer() {
+    this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
+    this.marker.setMap(this.map);
+  }
 
   constructor() {}
 
