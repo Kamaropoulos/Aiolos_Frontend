f16a9fb392aaa5ff8d336c74aad2150c0f30cddd
Konstantinos Kamaropoulos <kamaropoulos@outlook.com>
2020-03-02 16:15:07 +0200

    feat: Remove boilerplate Quotes service and functionality from the home page

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,7 +6,6 @@ import { CoreModule } from '@app/core';
 import { SharedModule } from '@app/shared';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { QuoteService } from './quote.service';
 
 @NgModule({
   imports: [CommonModule, TranslateModule, CoreModule, SharedModule, HomeRoutingModule],
